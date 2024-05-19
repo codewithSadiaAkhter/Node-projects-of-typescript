9324353e1974a4db7946799a852d69ef66811a30
SadiaAkhter <sadiakhan200599@gmail.com>
2024-05-19 14:18:35 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,2 @@
+var personName = "Zeeshan Ahmed";
+console.log("Hello", personName, "Would you like to learn some python today?");
---
+++ main.ts
@@ -0,0 +1,3 @@
+let personName = "Zeeshan Ahmed";
+
+console.log("Hello", personName, "Would you like to learn some python today?");
